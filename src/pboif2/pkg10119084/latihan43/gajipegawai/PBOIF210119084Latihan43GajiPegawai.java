e5da429bc0533f6c4259c0fe66727a2e275d9594
idrismerdefi <67642938+idrismerdefi@users.noreply.github.com>
2020-11-13 11:00:36 +0700

    Update PBOIF210119084Latihan43GajiPegawai.java

--- src/pboif2/pkg10119084/latihan43/gajipegawai/PBOIF210119084Latihan43GajiPegawai.java
+++ src/pboif2/pkg10119084/latihan43/gajipegawai/PBOIF210119084Latihan43GajiPegawai.java
@@ -4,10 +4,11 @@
  * and open the template in the editor.
  */
 package pboif2.pkg10119084.latihan43.gajipegawai;
-
 /**
- *
- * @author MERDEFI
+Nama : Muhammad Idris Merdefi
+Kelas : PBOIF2
+NIM : 10119084
+Deskripsi Program : Program ini berisi program untuk menampilkan perhitungan gaji pegawai
  */
 public class PBOIF210119084Latihan43GajiPegawai {
 
